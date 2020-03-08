ebbe683367605755a49af7ba421945e484d02c64
Olga <vashchyshynolga@gmail.com>
2020-03-08 13:48:55 -0400

    Add !!!!!!!

--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -7,7 +7,7 @@ public class MainPageTests extends BaseUI {
 
     @Test
     public void test() throws InterruptedException{
-
+        System.out.println("!!!!!!!");
         Thread.sleep(5000);
         WebElement video= driver.findElement(Locators.VIDEO_FIELD);
         driver.switchTo().frame(video);
