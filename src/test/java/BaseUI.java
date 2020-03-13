13544ff1fb9930cdc4923e51a334f2e682686bd2
Olga <vashchyshynolga@gmail.com>
2020-03-13 16:44:48 -0400

    Add new drivers, new properties to pom.xml
    changed Before Method

--- src/test/java/BaseUI.java
+++ src/test/java/BaseUI.java
@@ -1,9 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
+
+import java.lang.reflect.Method;
 
 public class BaseUI {
     WebDriver driver;
@@ -14,7 +20,31 @@ public class BaseUI {
     SoftAssert softAssert= new SoftAssert();
 
     @BeforeMethod
-    public void setUp (){
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browser, Method method){
+        // Check if parameter passed from TestNG is 'firefox'
+        if (browser.equalsIgnoreCase("firefox")) {
+            // Create firefox instance
+            System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        // Check if parameter passed as 'chrome'
+        else if (browser.equalsIgnoreCase("chrome")) {
+            // Set path to chromedriver.exe
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            // Create chrome instance
+            driver = new ChromeDriver();
+            driver.get("chrome://settings/clearBrowserData");
+        } else if (browser.equalsIgnoreCase("IE")) {
+            System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+            driver.manage().deleteAllCookies();
+        } else {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get("chrome://settings/clearBrowserData");
+        }
+
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver() ;
